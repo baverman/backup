8513c30f723019319c331678d9eec32fbb57c2c0
Anton Bobrov <a.bobrov@rambler-co.ru>
2012-07-16 17:56:30 +0400

    script to update all tracking git branches

--- update.sh
+++ update.sh
@@ -11,7 +11,7 @@ copy_files .config/bmpanel2 '*'
 copy_files .themes/Awesome '*'
 copy_files .local/share/bmpanel2/themes/awesome '*'
 
-for file in box-session.sh bpython colors.sh dbus-env opera pipdc rx show-sizes stg-flush title xxx check-mail; do
+for file in box-session.sh bpython colors.sh dbus-env opera pipdc rx show-sizes stg-flush title xxx check-mail git-pull-all; do
     cp ~/bin/$file bin/
 done
 
