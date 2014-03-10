47f40f95f0105902b6a9e63ce065d5738b40d914
Anton Bobrov <bobrov@vl.ru>
2014-03-11 02:47:02 +0400

    xinit: external monitor. openbox: <center> workaround

--- bin/box-session.sh
+++ bin/box-session.sh
@@ -1,10 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-notipy &
 bmpanel2 &
 gatotray &
 orcsome -l /tmp/orcsome.log &
-feh --bg-center wallpapers/story.png
 #xcompmgr -cC -t-5 -l-5 -r4.2 -o.55 &
 
 exec openbox
