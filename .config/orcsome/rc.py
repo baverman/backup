fa803715acef9c5e66b1a3ec146b00a09f2e1962
Anton Bobrov <bobrov@vl.ru>
2014-04-26 01:52:11 +0400

    orcsomerc: changes for new API

--- .config/orcsome/rc.py
+++ .config/orcsome/rc.py
@@ -7,91 +7,55 @@ TERMINAL = 'urxvtc -title terminal'
 
 wm = get_wm()
 
-wm.on_key('Shift+Mod+r')(
-    restart)
+wm.on_key('Shift+Mod+r')(restart)
+wm.on_key('Shift+Mod+c')(close)
+wm.on_key('Shift+Mod+e')(spawn('external-monitor'))
+wm.on_key('Ctrl+Alt+x')(spawn(TERMINAL))
+wm.on_key('Mod+e')(spawn('menu.sh'))
+wm.on_key('Mod+bracketleft')(spawn('mpc volume -2'))
+wm.on_key('Mod+bracketright')(spawn('mpc volume +2'))
+wm.on_key('Mod+period')(spawn('mpc next'))
+wm.on_key('Mod+comma')(spawn('mpc prev'))
+wm.on_key('Mod+slash')(spawn('mpc-forward'))
+wm.on_key('Mod+grave')(spawn('mpc-trash'))
+wm.on_key('Mod+apostrophe')(spawn('mpc-add-tag -D trash listen -A blade'))
+wm.on_key('Mod+semicolon')(spawn('mpc-add-tag -D trash blade -A listen'))
+wm.on_key('Mod+p')(spawn('mpc toggle'))
+wm.on_key('XF86_MonBrightnessUp')(spawn('xbacklight -inc 15'))
+wm.on_key('XF86_MonBrightnessDown')(spawn('xbacklight -dec 1'))
+wm.on_key('XF86_PowerOff')(spawn('sudo pm-suspend'))
+wm.on_key('Mod+i')(spawn_or_raise('urxvtc -name weechat -e weechat-curses', name='weechat'))
+wm.on_key('Mod+l')(spawn_or_raise('urxvtc -g 100x30 -name ranger -e ranger', name='ranger'))
 
-wm.on_key('Shift+Mod+c')(
-    close)
-
-wm.on_key('Shift+Mod+e')(
-    spawn('external-monitor'))
-
-wm.on_key('Ctrl+Alt+x')(
-    spawn(TERMINAL))
-
-wm.on_key('Mod+e')(
-    spawn('menu.sh'))
-
-wm.on_key('Mod+bracketleft')(
-    spawn('mpc volume -2'))
-
-wm.on_key('Mod+bracketright')(
-    spawn('mpc volume +2'))
-
-wm.on_key('Mod+period')(
-    spawn('mpc next'))
-
-wm.on_key('Mod+comma')(
-    spawn('mpc prev'))
-
-wm.on_key('Mod+slash')(
-    spawn('mpc-forward'))
-
-wm.on_key('Mod+grave')(
-    spawn('mpc-trash'))
-
-wm.on_key('Mod+apostrophe')(
-    spawn('mpc-add-tag -D trash listen -A blade'))
-
-wm.on_key('Mod+semicolon')(
-    spawn('mpc-add-tag -D trash blade -A listen'))
-
-wm.on_key('Mod+p')(
-    spawn('mpc toggle'))
-
-wm.on_key('XF86_MonBrightnessUp')(
-    spawn('xbacklight -inc 15'))
-
-wm.on_key('XF86_MonBrightnessDown')(
-    spawn('xbacklight -dec 1'))
-
-wm.on_key('XF86_PowerOff')(
-    spawn('sudo pm-suspend'))
-
-wm.on_key('Mod+i')(
-    spawn_or_raise('urxvtc -name weechat -e weechat-curses', name='weechat'))
-
-@wm.on_key('Ctrl+Mod+space')
-def maximize_window():
-    cw = wm.current_window
-    state = wm.get_window_state(cw)
-    if state.maximized_vert and state.maximized_horz:
-        wm.set_window_state(cw, vmax=False, hmax=False, decorate=True, otaskbar=False)
-    else:
-        wm.set_window_state(cw, vmax=True, hmax=True, decorate=False, otaskbar=True)
-
-wm.on_key('Mod+l')(
-    spawn_or_raise('urxvtc -g 100x30 -name ranger -e ranger', name='ranger'))
 
 ################################
 # Handle quick apps window close
 def bind_restore_focus(desktop, window):
     @wm.on_destroy(wm.event_window)
     def inner():
-        wm.set_current_desktop(desktop)
+        wm.activate_desktop(desktop)
 restore_focus = {'on_create':bind_restore_focus}
 
-wm.on_key('Ctrl+Alt+p')(
-    spawn_or_raise('urxvtc -name ncmpcpp -e ncmpcpp', name='ncmpcpp', **restore_focus))
+wm.on_key('Ctrl+Alt+p')(spawn_or_raise(
+    'urxvtc -name ncmpcpp -e ncmpcpp', name='ncmpcpp', **restore_focus))
 
-wm.on_key('Mod+n')(
-    spawn_or_raise('urxvtc -title mutt -name mutt -e startmutt.sh', name='mutt', **restore_focus))
+wm.on_key('Mod+n')(spawn_or_raise(
+    'urxvtc -title mutt -name mutt -e startmutt.sh', name='mutt', **restore_focus))
 
-wm.on_key('Ctrl+Alt+m')(
-    spawn_or_raise('urxvtc -name alsamixer -e alsamixer', name='alsamixer', **restore_focus))
+wm.on_key('Ctrl+Alt+m')(spawn_or_raise(
+    'urxvtc -name alsamixer -e alsamixer', name='alsamixer', **restore_focus))
 
-wm.on_key('Mod+k')(
-    spawn_or_raise('urxvtc -name rtorrent -e transmission-remote-cli', name='rtorrent', **restore_focus))
+wm.on_key('Mod+k')(spawn_or_raise(
+    'urxvtc -name rtorrent -e transmission-remote-cli', name='rtorrent', **restore_focus))
+
+
+@wm.on_key('Ctrl+Mod+space')
+def maximize_window():
+    w = wm.current_window
+    if w.maximized_vert and w.maximized_horz:
+        wm.set_window_state(w, vmax=False, hmax=False, decorate=True, otaskbar=False)
+    else:
+        wm.set_window_state(w, vmax=True, hmax=True, decorate=False, otaskbar=True)
 
 
 ##########################
@@ -100,11 +64,11 @@ wm.on_key('Mod+k')(
 def toggle_console():
     cd = wm.current_desktop
     if cd == 1:
-        wm.set_current_desktop(0)
+        wm.activate_desktop(0)
     else:
         clients = wm.find_clients(wm.get_clients(), cls="URxvt")
         if clients:
-            wm.set_current_desktop(1)
+            wm.activate_desktop(1)
         else:
             spawn(TERMINAL)()
 
@@ -121,20 +85,20 @@ def app_rules(w):
     maximize = None
     otaskbar = None
 
-    if wm.is_match(w, name='vial'):
+    if w.matches(name='vial'):
         maximize = True
         decorate = False
-    elif wm.is_match(w, name='ranger'):
+    elif w.matches(name='ranger'):
         otaskbar = False
-    elif wm.is_match(w, name='Navigator', cls='Firefox'):
+    elif w.matches(name='Navigator', cls='Firefox'):
         decorate = False
-    elif wm.is_match(w, cls='URxvt'):
+    elif w.matches(cls='URxvt'):
         desktop = 1
         decorate = False
         maximize = True
-    elif wm.is_match(w, name='pinentry', cls='Pinentry'):
+    elif w.matches(name='pinentry', cls='Pinentry'):
         desktop = -1
-    elif wm.is_match(w, cls='bmpanel'):
+    elif w.matches(cls='bmpanel'):
         return
 
     wm.change_window_desktop(w, desktop)
@@ -145,7 +109,7 @@ def app_rules(w):
 
     cd = wm.current_desktop
     if desktop >=0 and desktop != cd:
-        wm.set_current_desktop(desktop)
+        wm.activate_desktop(desktop)
 
 
 #########################
@@ -156,8 +120,7 @@ def on_create():
         return
 
     w = wm.event_window
-    wd = wm.get_window_desktop(w)
-    if wd is None:
+    if w.desktop is None:
         @wm.on_property_change(w, '_NET_WM_DESKTOP')
         def property_was_set():
             property_was_set.remove()
