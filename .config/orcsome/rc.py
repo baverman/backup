683f8dbf4813952808aa22731b08fef4cdb89625
Anton Bobrov <bobrov@vl.ru>
2014-04-26 17:49:12 +0400

    orcsomerc: changes for new API

--- .config/orcsome/rc.py
+++ .config/orcsome/rc.py
@@ -1,5 +1,5 @@
 from orcsome import get_wm
-from orcsome.actions import *
+from orcsome.actions import spawn
 
 import rsi
 
@@ -7,46 +7,43 @@ TERMINAL = 'urxvtc -title terminal'
 
 wm = get_wm()
 
-wm.on_key('Shift+Mod+r')(restart)
-wm.on_key('Shift+Mod+c')(close)
-wm.on_key('Shift+Mod+e')(spawn('external-monitor'))
-wm.on_key('Ctrl+Alt+x')(spawn(TERMINAL))
-wm.on_key('Mod+e')(spawn('menu.sh'))
-wm.on_key('Mod+bracketleft')(spawn('mpc volume -2'))
-wm.on_key('Mod+bracketright')(spawn('mpc volume +2'))
-wm.on_key('Mod+period')(spawn('mpc next'))
-wm.on_key('Mod+comma')(spawn('mpc prev'))
-wm.on_key('Mod+slash')(spawn('mpc-forward'))
-wm.on_key('Mod+grave')(spawn('mpc-trash'))
-wm.on_key('Mod+apostrophe')(spawn('mpc-add-tag -D trash listen -A blade'))
-wm.on_key('Mod+semicolon')(spawn('mpc-add-tag -D trash blade -A listen'))
-wm.on_key('Mod+p')(spawn('mpc toggle'))
-wm.on_key('XF86_MonBrightnessUp')(spawn('xbacklight -inc 15'))
-wm.on_key('XF86_MonBrightnessDown')(spawn('xbacklight -dec 1'))
-wm.on_key('XF86_PowerOff')(spawn('sudo pm-suspend'))
-wm.on_key('Mod+i')(spawn_or_raise('urxvtc -name weechat -e weechat-curses', name='weechat'))
-wm.on_key('Mod+l')(spawn_or_raise('urxvtc -g 100x30 -name ranger -e ranger', name='ranger'))
+wm.on_key('Shift+Mod+r').restart()
+wm.on_key('Shift+Mod+c').close()
+wm.on_key('Shift+Mod+e').spawn('external-monitor')
+wm.on_key('Ctrl+Alt+x').spawn(TERMINAL)
+wm.on_key('Mod+e').spawn('menu.sh')
+wm.on_key('Mod+bracketleft').spawn('mpc volume -2')
+wm.on_key('Mod+bracketright').spawn('mpc volume +2')
+wm.on_key('Mod+period').spawn('mpc next')
+wm.on_key('Mod+comma').spawn('mpc prev')
+wm.on_key('Mod+slash').spawn('mpc-forward')
+wm.on_key('Mod+grave').spawn('mpc-trash')
+wm.on_key('Mod+apostrophe').spawn('mpc-add-tag -D trash listen -A blade')
+wm.on_key('Mod+semicolon').spawn('mpc-add-tag -D trash blade -A listen')
+wm.on_key('Mod+p').spawn('mpc toggle')
+wm.on_key('XF86_MonBrightnessUp').spawn('xbacklight -inc 15')
+wm.on_key('XF86_MonBrightnessDown').spawn('xbacklight -dec 1')
+wm.on_key('XF86_PowerOff').spawn('sudo pm-suspend')
+wm.on_key('Mod+i').spawn_or_raise('urxvtc -name weechat -e weechat-curses', name='weechat')
+wm.on_key('Mod+l').spawn_or_raise('urxvtc -g 100x30 -name ranger -e ranger', name='ranger')
 
 
 ################################
 # Handle quick apps window close
-def bind_restore_focus(desktop, window):
-    @wm.on_destroy(wm.event_window)
-    def inner():
-        wm.activate_desktop(desktop)
-restore_focus = {'on_create':bind_restore_focus}
+restore_focus = {'on_create': lambda desktop, window:
+    wm.on_destroy(wm.event_window).activate_desktop(desktop)}
 
-wm.on_key('Ctrl+Alt+p')(spawn_or_raise(
-    'urxvtc -name ncmpcpp -e ncmpcpp', name='ncmpcpp', **restore_focus))
+wm.on_key('Ctrl+Alt+p').spawn_or_raise(
+    'urxvtc -name ncmpcpp -e ncmpcpp', name='ncmpcpp', **restore_focus)
 
-wm.on_key('Mod+n')(spawn_or_raise(
-    'urxvtc -title mutt -name mutt -e startmutt.sh', name='mutt', **restore_focus))
+wm.on_key('Mod+n').spawn_or_raise(
+    'urxvtc -title mutt -name mutt -e startmutt.sh', name='mutt', **restore_focus)
 
-wm.on_key('Ctrl+Alt+m')(spawn_or_raise(
-    'urxvtc -name alsamixer -e alsamixer', name='alsamixer', **restore_focus))
+wm.on_key('Ctrl+Alt+m').spawn_or_raise(
+    'urxvtc -name alsamixer -e alsamixer', name='alsamixer', **restore_focus)
 
-wm.on_key('Mod+k')(spawn_or_raise(
-    'urxvtc -name rtorrent -e transmission-remote-cli', name='rtorrent', **restore_focus))
+wm.on_key('Mod+k').spawn_or_raise(
+    'urxvtc -name rtorrent -e transmission-remote-cli', name='rtorrent', **restore_focus)
 
 
 @wm.on_key('Ctrl+Mod+space')
@@ -70,13 +67,13 @@ def toggle_console():
         if clients:
             wm.activate_desktop(1)
         else:
-            spawn(TERMINAL)()
+            spawn(TERMINAL)
 
 
-@wm.on_create(cls='URxvt')
+@wm.on_manage(cls='URxvt')
 def bind_urxvt_keys():
-    wm.on_key(wm.event_window, 'Shift+Right')(focus_next)
-    wm.on_key(wm.event_window, 'Shift+Left')(focus_prev)
+    wm.on_key(wm.event_window, 'Shift+Right').focus_next()
+    wm.on_key(wm.event_window, 'Shift+Left').focus_prev()
 
 
 def app_rules(w):
@@ -116,9 +113,6 @@ def app_rules(w):
 # Apply application rules
 @wm.on_create
 def on_create():
-    if wm.startup:
-        return
-
     w = wm.event_window
     if w.desktop is None:
         @wm.on_property_change(w, '_NET_WM_DESKTOP')
