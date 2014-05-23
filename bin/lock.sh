e1251b35f93a90e04bcbdb341ffa0e06a65bdd84
Anton Bobrov <bobrov@vl.ru>
2014-05-24 02:09:14 +0400

    lock.sh: turn off indicator

--- bin/lock.sh
+++ bin/lock.sh
@@ -2,5 +2,5 @@
 import -window root /tmp/boo.png
 # convert /tmp/boo.png -blur 40x10 /tmp/boo.png
 convert /tmp/boo.png -filter Gaussian -resize 25% -define filter:sigma=1.25 -resize 400% /tmp/boo.png
-i3lock -i /tmp/boo.png
+i3lock -u -i /tmp/boo.png
 rm /tmp/boo.png
