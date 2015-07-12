76e16c35c84a553dc0e1f65ff3e82b5bc76139b8
Anton Bobrov <bobrov@vl.ru>
2015-07-12 09:55:05 +0300

    Parametrized font for menu.sh

--- bin/menu.sh
+++ bin/menu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
-exe=`( cat ~/bin/menu.lst && dmenu_path ) | dmenu -nb '#222222' -nf '#aaaaaa' -sb '#535d6c' -sf '#ffffff' -fn 'Helvetica-11'`
+font=${DMENU_FONT:-"Helvetica-11"}
+exe=`( cat ~/bin/menu.lst && dmenu_path ) | dmenu -nb '#222222' -nf '#aaaaaa' -sb '#535d6c' -sf '#ffffff' -fn "$font"`
 exitcode=$?
 
 [ $exitcode = 0 ] && exec $exe
